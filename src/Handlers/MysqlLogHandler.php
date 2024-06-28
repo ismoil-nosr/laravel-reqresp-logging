70c095ae50ac0c8a03d91e034e9bebb3c4d32b7b
Ismoil <ismoil.nosr@gmail.com>
2024-06-28 11:18:19 +0500

    fix handler

--- src/Handlers/MysqlLogHandler.php
+++ src/Handlers/MysqlLogHandler.php
@@ -4,19 +4,21 @@ namespace IsmoilNosr\ReqrespLogger\Handlers;
 
 use Illuminate\Support\Facades\DB;
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
+use function json_encode;
 
 class MysqlLogHandler extends AbstractProcessingHandler
 {
-    protected function write(array $record): void
+    protected function write(LogRecord $record): void
     {
         DB::table('reqresp_logs')->insert([
-            'message' => $record['message'],
-            'context' => json_encode($record['context']),
-            'level' => $record['level'],
-            'level_name' => $record['level_name'],
-            'channel' => $record['channel'],
-            'datetime' => $record['datetime']->format('Y-m-d H:i:s'),
-            'extra' => json_encode($record['extra']),
+            'message' => $record->message,
+            'context' => json_encode($record->context),
+            'level' => $record->level->value,
+            'level_name' => $record->level->name,
+            'channel' => $record->channel,
+            'datetime' => $record->datetime->format('Y-m-d H:i:s'),
+            'extra' => json_encode($record->extra),
         ]);
     }
 }
